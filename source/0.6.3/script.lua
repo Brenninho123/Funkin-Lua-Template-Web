4ee53621184c99930496156d056d5fbde3c135fd
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-13 18:27:33 -0300

    Atualizar o script.lua

--- source/0.6.3/script.lua
+++ source/0.6.3/script.lua
@@ -1,4 +1,4 @@
-local -- essentially a configuration of a certain thing --
+local -- essentially a configuration of a certain thing
 
 function onCreate() -- the beginning of the script
 end -- the end of the script
