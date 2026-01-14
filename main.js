c2bc5187e2ab6035c88a7e9df5e31b2d393805aa
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-14 00:16:14 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1280,
+    width: 1200,
     height: 800,
     webPreferences: {
-      contextIsolation: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
-  win.loadFile("index.html");
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
